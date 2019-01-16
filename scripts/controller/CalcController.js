ec492fe10e463ce6767a8f4599e779ddd652755b
Cris Stef <cristiane.souza1234@hotmail.com>
2019-01-16 16:15:12 -0300

    Arquivos

---
+++ scripts/controller/CalcController.js
@@ -0,0 +1,59 @@
+class CalcController {
+
+    constructor() {
+
+        this._locale = 'pt-BR';
+        this._displayCalcEl = document.querySelector("#display");
+        this._dateEl = document.querySelector("#data");
+        this._timeEl = document.querySelector("#hora");
+        this._currentDate;
+        this.initialize();
+    }
+
+    initialize() {
+        
+        let interval = setInterval(()=>{
+
+            this.displayDate = this.currentDate.toLocaleDateString(this._locale);
+            this.displayTime = this.currentDate.toLocaleTimeString(this._locale);
+        }, 1000);
+
+        setTimeout(()=>{
+
+            clearInterval(interval);
+
+        }, 10000);
+    }
+
+    get displayCalc() {
+        return this._displayCalcEl.innerHTML;
+    }
+
+    set displayCalc(value) {
+        this._displayCalcEl.innerHTML = value;
+    }
+
+    get displayTime() {
+        return this._timeEl.innerHTML;
+    }
+
+    set displayTime(value) {
+        this._timeEl.innerHTML = value;
+    }
+
+    get displayDate() {
+        return this._dateEl.innerHTML;
+    }
+
+    set displayDate(value) {
+        this._dateEl.innerHTML = value;
+    }
+
+    get currentDate() {
+        return new Date();
+    }
+
+    set currentDate(value) {
+        this.currentDate = valor;
+    }
+}
