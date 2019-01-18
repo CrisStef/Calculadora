54ad6aad857c69eb91e88eb80603a84aba96ee41
Cris Stef <cristiane.souza1234@hotmail.com>
2019-01-18 16:41:27 -0300

    Botão Porcento

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -1,5 +1,8 @@
 class CalcController {
 
+    /**
+     * Método construtor da classe
+     */
     constructor() {
 
         this._operation = [];
@@ -12,17 +15,29 @@ class CalcController {
         this.initButtonsEvents();
     }
 
+    /**
+     * Chamado ao inicializar a página
+     */
     initialize() {
         
         this.setDisplayDateTime();
         
+        //Executa o método setDisplayDateTime a cada segundo
         let interval = setInterval(()=>{
 
             this.setDisplayDateTime();
             
         }, 1000);
+
+        this.setLastNumberToDisplay();
     }
 
+    /**
+     * Percorre um array de eventos e executa o evento encontrado
+     * @param {*} element elemento que está sendo passado
+     * @param {*} events lista de elementos
+     * @param {*} fn função a ser executada
+     */
     addEventListenerAll(element, events, fn) {
 
         events.split(' ').forEach(event => {
@@ -32,26 +47,51 @@ class CalcController {
 
     }
 
+    /**
+     * Esvazia o array
+     */
     clearAll() {
         this._operation = [];
+        this.setLastNumberToDisplay();
     }
 
+    /**
+     * Remove o último elemento do array
+     */
     clearEntry() {
         this._operation.pop();
+        this.setLastNumberToDisplay();
     }
 
+    /**
+     * Recupera a última operação inserida no array
+     */
     getLastOperation() {
         return this._operation[this._operation.length-1];
     }
 
+    /**
+     * Insere na última posição do array um valor, sobrescrevendo o valor anterior
+     * @param {*} value valor a ser adicionado no final do array
+     */
     setLastOperation(value) {
         this._operation[this._operation.length-1] = value;
     }
 
+    /**
+     * Verifica se valor passado é um operador 
+     * @param {*} value valor a ser verificado
+     */
     isOperator(value) {
         return (['+', '-', '*', '/', '%'].indexOf(value) > -1);
     }
 
+    /**
+     * Adiciona no array o valor 
+     * Verifica se o array tem 3 elementos, se tiver ele executa a função para calcular 
+     * Ex: n1 + n2 
+     * @param {*} value o valor a ser adicionado no array
+     */
     pushOperation(value) {
         this._operation.push(value);
 
@@ -61,31 +101,68 @@ class CalcController {
         }
     }
 
+    /**
+     * Realiza o calculo
+     */
     calc() {
 
-        let last = this._operation.pop();
+        let last = '';
 
-        let result = eval(this._operation.join(""));
+        if (this._operation.length > 3) {
+            let last = this._operation.pop(); //Remove do array o 4º elemento adicionado e o guarda para ser utilizado no próximo calculo
+        }
 
-        this._operation = [result, last];
+        let result = eval(this._operation.join("")); //eval realiza o calculo; join transforma o array em uma string
+
+        if (last == '%') {
+            let porcento = this._operation[0] * this._operation[2] / 100;
+            
+            this.setLastOperation(porcento);
+        } else {
+            this._operation = [result]; // limpa o array inserindo o resultado do calculo, e o 4º elemento digitado
+            if (last) this._operation.push(last);
+        }
+
+        this.setLastNumberToDisplay(); // atualiza os valores no display
     }
 
+    /**
+     * Recupera o último número inserido no array e exibe no display
+     */
     setLastNumberToDisplay() {
 
+        let lastNumber;
+
+        for (let i = this._operation.length -1; i >= 0; i--) {
+            if (!this.isOperator(this._operation[i])) {
+                lastNumber = this._operation[i];
+                break;
+            }
+        }
+
+        if (!lastNumber) lastNumber = 0;
+
+        this.displayCalc = lastNumber;
     }
 
+    /**
+     * Adiciona no array os valores
+     * @param {*} value 
+     */
     addOperation(value) {
 
-        if (isNaN(this.getLastOperation())) {
+        if (isNaN(this.getLastOperation())) { // isNan: função verifica se o valor não é um número
 
+            //Caso o operador do calculo seja alterado executa o metodo que altera a ultima posição do array, substituindo o valor
             if (this.isOperator(value)) {
                 this.setLastOperation(value);
                 
             } else if (isNaN(value)) {
                 console.log('Outra coisa ',value);
                 
-            } else {
+            } else { // caso não seja um operador matematico, ele inclui o numero no array
                 this.pushOperation(value);
+                this.setLastNumberToDisplay();
             }
         } else {
 
@@ -93,20 +170,26 @@ class CalcController {
                 this.pushOperation(value);
 
             } else {
+                //Concatena os números inseridos 
                 let newValue = this.getLastOperation().toString() + value.toString();
                 this.setLastOperation(parseInt(newValue));
 
                 this.setLastNumberToDisplay();
             }
         }
-
-        console.log(this._operation);
     }
 
+    /**
+     * Mensagem de erro
+     */
     setError() {
         this.displayCalc = "Error";
     }
 
+    /**
+     * Verifica qual o valor capturado no evento e executa a função correspondente a ele
+     * @param {*} value 
+     */
     execBtn (value) {
 
         switch (value) {
@@ -133,7 +216,7 @@ class CalcController {
                 this.addOperation('%');    
                 break;
             case 'igual':
-                    
+                this.calc();    
                 break;
             case 'ponto':
                 this.addOperation('.');
@@ -158,8 +241,12 @@ class CalcController {
         }
     }
 
+    /**
+     * Inicializa os eventos dos botões
+     */
     initButtonsEvents() {
 
+        //seleciona todos os elementos das classes e id's passados no parametro
         let buttons = document.querySelectorAll("#buttons > g, #parts > g");
 
         buttons.forEach((btn, index)=>{
@@ -171,6 +258,7 @@ class CalcController {
 
             });
 
+            //modifica cursor do mouse 
             this.addEventListenerAll(btn, "mouseover mouseup mousedown", e => {
 
                 btn.style.cursor = "pointer";
@@ -178,6 +266,9 @@ class CalcController {
         });
     }
 
+    /**
+     * Mostra no display a data e a hora
+     */
     setDisplayDateTime() {
 
         this.displayDate = this.currentDate.toLocaleDateString(this._locale, {
